bc9d5ed946b00ebc60a37b78141ef12f90c2a4ab
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 12:54:08 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,23 +1,22 @@
 var mongoose= require("mongoose");
 
 //Write your schema here with name of schema as userSchema
-const  userSchema=mongoose.schema({
+const userSchema = new mongoose.Schema({
     name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    pswd:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        enum:["buyer","seller"]
-    }
-})
+        type: String,
+        required: true
+       },
+       email:{
+        type: String,
+        required: true
+       },
+    pswd : {
+        type: String,
+        required: true
+        },
+    role : {
+        type: String
+        }
+});
 
 module.exports =mongoose.model("users",userSchema);
